0a1f07f0f7ac3b7c814b97c78d4853920d0c2713
Amin Khodabandeh <Amin Khodabandeh>
2017-04-08 17:30:50 +0800

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "@angular/material";
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     FormsModule,
     HttpModule
   ],
