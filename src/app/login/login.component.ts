26b0d4a2968e879220704c1a098a3c5c3c8ec589
Amin Khodabandeh <Amin Khodabandeh>
2017-04-09 01:24:51 +0800

    Fix Email validator

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,27 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { MdSnackBar } from "@angular/material";
 import { FormControl, Validators } from '@angular/forms';
 
-
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit {  
 
   constructor(public snackBar: MdSnackBar) { }
 
-  emailControl = new FormControl('', [Validators.required, Validators.pattern(EMAIL_REGEX)]);
+  password: string;
+  email: string;
+
+  validate(): boolean{
+    return (this.password && this.email && EMAIL_REGEX.exec(this.email) != null);    
+  }
 
   ngOnInit() {
   }
 
   tryLogin(): void{
-    this.snackBar.open('Login successful.', '', {
-      duration: 20000,
-    });
+    if(this.validate()){
+      this.snackBar.open('Login successful.', '', {
+        duration: 20000,
+      });
+    }
+    else{
+      this.snackBar.open('Invalid inputs', '', {
+        duration: 1000
+      });
+    }
   }
 }
